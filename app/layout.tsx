f2ea84a2e3b7098efd9a484a7b214533ebaee0f7
shamim-s <shamimibas@gmail.com>
2025-03-30 13:14:20 +0600

    Navber & Footer Added

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { APP_NAME } from "@/lib/constants";
+import Footer from "@/components/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} antialiased`}>{children}</body>
+      <Footer />
     </html>
   );
 }
--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,4 +1,6 @@
+import { Button } from "@/components/ui/button";
 import { APP_NAME } from "@/lib/constants";
+import { ShoppingCart, UserIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -6,18 +8,32 @@ const Header = () => {
   return (
     <header className="w-full border-b">
       <div className="wrapper flex-between">
-        <Link href={"/"} className="flex-start">
-          <Image
-            src={"/images/logo.svg"}
-            alt={`${APP_NAME}`}
-            width={48}
-            height={48}
-            priority={true}
-          />
-          <span className="hidden text-bold lg:block text-2xl ml-3">
-            {APP_NAME}
-          </span>
-        </Link>
+        <div className="flex-start">
+          <Link href={"/"} className="flex-start">
+            <Image
+              src={"/images/logo.svg"}
+              alt={`${APP_NAME}`}
+              width={48}
+              height={48}
+              priority={true}
+            />
+            <span className="hidden text-bold lg:block text-2xl ml-3">
+              {APP_NAME}
+            </span>
+          </Link>
+        </div>
+        <div className="space-x-2">
+          <Button asChild variant={"ghost"}>
+            <Link href={"/cart"}>
+              <ShoppingCart /> Cart
+            </Link>
+          </Button>
+          <Button asChild variant={"ghost"}>
+            <Link href={"/signin"}>
+              <UserIcon /> Signin
+            </Link>
+          </Button>
+        </div>
       </div>
     </header>
   );
